f5be07dc153fc0c73b4c7744e930b66e0cedfa81
pong34811 <pong34811@gmail.com>
2025-02-06 11:59:06 +0700

    Refactor ResetPassword component layout and styling; enhance user experience with card design and improved text clarity

--- jathai_frontend/src/Pages/ResetPassword.js
+++ jathai_frontend/src/Pages/ResetPassword.js
@@ -21,34 +21,33 @@ function ResetPassword({ resetPassword }) {
     return <Navigate to={"../"}></Navigate>;
   }
   return (
-    <div className="main-box">
-      <h2 className="text-center mb-4">Reset Password</h2>
-      <h5 className="text-center mb-4">
-        Please input your registered email. The link for set your new password
-        will be stent into your email
-      </h5>
-      <form className="mb-3" onSubmit={(e) => handlingSubmit(e)}>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email address
-          </label>
-          <input
-            name="email"
-            value={email}
-            onChange={(e) => handlingInput(e)}
-            type="email"
-            className="form-control"
-            id="email"
-            placeholder="name@example.com"
-          />
-        </div>
-        <div className="d-grid gap-2">
-          <button className="btn btn-primary" type="submit">
-            Send Link
-          </button>
-        </div>
-      </form>
-    </div>
+    <div className="container d-flex justify-content-center align-items-center" style={{ marginTop: "180px" }}> 
+  <div className="card shadow-lg p-4" style={{ width: '100%', maxWidth: '400px' }}>
+    <h2 className="text-center mb-4">Reset Password</h2>
+    <h5 className="text-center mb-4">
+      Please input your registered email. The link for setting your new password
+      will be sent to your email.
+    </h5>
+    <form className="mb-3" onSubmit={(e) => handlingSubmit(e)}>
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label">Email address</label>
+        <input
+          name="email"
+          value={email}
+          onChange={(e) => handlingInput(e)}
+          type="email"
+          className="form-control"
+          id="email"
+          placeholder="name@example.com"
+        />
+      </div>
+      <div className="d-grid gap-2">
+        <button className="btn btn-primary" type="submit">Send Link</button>
+      </div>
+    </form>
+  </div>
+</div>
+
   );
 }
 
