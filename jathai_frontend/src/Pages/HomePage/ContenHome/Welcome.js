6dae63f921c87979c08871a61ee006340fdd4ddf
Kanya Lenovo <pong34811@gmail.com>
2025-02-08 00:41:34 +0700

    Update login API endpoint to use production URL

--- jathai_frontend/src/Pages/HomePage/ContenHome/Welcome.js
+++ jathai_frontend/src/Pages/HomePage/ContenHome/Welcome.js
@@ -13,7 +13,7 @@ function Welcome() {
 
     try {
       // ส่งคำร้องไปที่ API สำหรับ login
-      const response = await fetch("http://localhost:8000/dj-rest-auth/login/", {
+      const response = await fetch("https://api.janhai.space/dj-rest-auth/login/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
