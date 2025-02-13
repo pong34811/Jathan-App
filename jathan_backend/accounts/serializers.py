8d2c73cfe2cc8944ff77a1f50b6f53e6545f67c4
pong34811 <pong34811@gmail.com>
2025-02-13 20:02:59 +0700

    Add email notification fields to CustomUserSerializer; include fields for board and task notifications

--- jathan_backend/accounts/serializers.py
+++ jathan_backend/accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import CustomUserModel
 
+
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUserModel
@@ -8,6 +9,8 @@ class CustomUserSerializer(serializers.ModelSerializer):
             'id', 'email', 'first_name', 'last_name', 'line_user_id',
             'is_notify_create_board', 'is_notify_update_board', 'is_notify_delete_board',
             'is_notify_create_task', 'is_notify_update_task', 'is_notify_delete_task',
+            "is_email_notify_create_board", "is_email_notify_update_board", "is_email_notify_delete_board",
+            "is_email_notify_create_task", "is_email_notify_update_task", "is_email_notify_delete_task",
             'is_active', 'is_staff', 'date_joined', 'last_login'
         ]
         read_only_fields = ['id', 'date_joined', 'last_login']
