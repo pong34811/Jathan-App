36567df3863374cc8c738fcd0c245dbf86412e23
pong34811 <pong34811@gmail.com>
2025-02-15 00:15:14 +0700

    asdasd

--- jathai_frontend/src/Apis/ConfigApis.js
+++ jathai_frontend/src/Apis/ConfigApis.js
@@ -1,10 +1,7 @@
 export const URL_AUTH = {
-    BoardAPI: "https://api.janhai.space/api/boards/",
-    ListsAPI: "https://api.janhai.space/api/lists/",
-    ReorderListsAPI: "https://api.janhai.space/api/lists/reorder/",
-    TasksAPI: "https://api.janhai.space/api/tasks/",
-
-    UsersAPI: "https://api.janhai.space/dj-rest-auth/user/", // เพิ่ม URL สำหรับ Users
-};
-
-  
+  BoardAPI: "https://api.janhai.space/api/boards/",
+  ListsAPI: "https://api.janhai.space/api/lists/",
+  ReorderListsAPI: "https://api.janhai.space/api/lists/reorder/", // แก้เป็น /api/lists/reorder/
+  TasksAPI: "https://api.janhai.space/api/tasks/",
+  UsersAPI: "https://api.janhai.space/dj-rest-auth/user/", // เพิ่ม URL สำหรับ Users
+  UserDetailAPI: "https://api.janhai.space/user/",
