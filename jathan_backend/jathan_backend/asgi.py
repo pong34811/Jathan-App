7474677197f772aa9a98b0c756bf84e5cd83fed8
Kanya Lenovo <pong34811@gmail.com>
2025-02-15 23:56:14 +0700

    refactor ASGI and WSGI configurations for Vercel deployment

--- jathan_backend/jathan_backend/asgi.py
+++ jathan_backend/jathan_backend/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+from jathan_backend.asgi import application
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jathan_backend.settings')
 
 application = get_asgi_application()
+
+handler = application
--- jathan_backend/jathan_backend/wsgi.py
+++ jathan_backend/jathan_backend/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from jathan_backend.wsgi import application
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jathan_backend.settings')
 
 application = get_wsgi_application()
+
+handler = application
